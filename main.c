cdf59aef6e5b7fb1c635ed536437cb6b79692cc5
anisdjl <adjelili954@gmail.com>
2026-02-07 23:57:44 +0100

    initial commit

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <pthread.h>
 
-
+void*	routine()
+{
+	printf("testing threads\n");
+	return NULL;
+}
 int main(void)
 {
+	pthread_t t1;
 
+	pthread_create(&t1, NULL, &routine, NULL);
+	pthread_join(t1, NULL);
+
+	return 0;
 }
