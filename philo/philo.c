fb30bf8d39386a5ff4d9c939c3d7dcefb5608bbb
anisdjl <adjelili954@gmail.com>
2026-02-14 16:43:32 +0100

    begin

---
+++ philo/philo.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adjelili <adjelili@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/14 14:25:04 by adjelili          #+#    #+#             */
+/*   Updated: 2026/02/14 16:14:34 by adjelili         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int main(int argc, char **argv)
+{
+	t_params	*params;
+
+	if (argc < 5 || argc > 6)
+		return (0);
+	params = malloc(sizeof(t_params));
+	init_struct(params, argc, argv);
+}
+
+void	init_struct(t_params *params, int argc, char **argv)
+{
+	params->philo = malloc(sizeof(t_philo));
+	if (!params->philo)
+		free(params);
+	params->nb_philo = argv[1];
+	params->time_to_die = argv[2];
+	params->time_to_eat = argv[3];
+	params->time_to_sleep = argv[4];
+	if (argc == 6)
+		params->notepme = argv[5];
+	else
+		params->notepme = 0;
+}
---
+++ philo/philo.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adjelili <adjelili@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/14 14:23:29 by adjelili          #+#    #+#             */
+/*   Updated: 2026/02/14 15:53:41 by adjelili         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+
+typedef struct s_philo
+{
+	int				pos_of_philo;
+	struct philo	*next;
+	struct philo	*prev;
+}	t_philo;
+
+typedef struct s_params
+{
+	int		nb_philo;
+	int		time_to_die;
+	int		time_to_eat;
+	int		time_to_sleep;
+	int		notepme;
+	t_philo	*philo;
+}	t_params;
+
+#endif
